5ebde681b7e9a8533192237bcb68a203618c3e50
Igor Afonso <igor.henrique.afonso99@gmail.com>
2023-09-12 20:39:10 -0300

    First Commit

---
+++ lib/app/app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../presentation/widget/my_homepage_state.dart';
+import '../presentation/widget/my_app_state.dart';
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
